80a52b9499fc802cdb443641243ee207bb06fb10
ashawinanand-hash <ashawinanand@gmail.com>
2026-04-06 23:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//console.log("ASHWIN");
+//var name="ASHWIN";
+//name="ANKUSH";
+//console.log(name);
+//let has block scope
+// var nam be used multipls times with same varibles and donot show error
+//var has global scope
+//but let shows error if we use same variable name multiple times
+{
+    let a=5;
+    let b=8;
+    let sum=a+b;
+    console.log(sum);
+}
+//console.log(sum);
+// if we put something in const variable so we can,t upadate it unlike let and var; if we try to do that it shows error
+//example
+//const name="ASHWIN";
+//name="TIWARI";
+//const me hume declartion+initilosation ek sath krna h 
+//example:-
+//const name; this shows error
+//but const name="ashwin"; yeh error nhi dega
+// in var we have 7 data types
+//data types primitive and non primitive
+// primitive has 7 data types
+//null number boolean string undefined symbol bigint
+//var a=undefined; example
+// in object (non primitive) stores has key value pairs
+//keywords that are used to declare variables var,let,const;
+// used to identify data type
+//example:-
+//let name="ashwin";
+//console.log(typeof name);
